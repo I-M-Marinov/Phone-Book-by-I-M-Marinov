0a4a87c7bb71f46775ea815a30b9aa6deb73279f
I-M-Marinov <lcfrrr@gmail.com>
2024-03-13 09:51:47 +0200

    Added features
    1. The program now loads information from an Excel sheet ( reads, writes and deletes new entries as well  ).
    
    2. Moved the buttons and expanded the text fields
    
    3. Added a custom icon to the GUI, that also has a link attached that takes the user to the repository.
    
    4. Extra link added that takes the user to the source code of the project
    
    5. Clicking on an empty row no longer shows an Unhandled exception

--- Phone-Book-by-I-M-Marinov/Form1.Designer.cs
+++ Phone-Book-by-I-M-Marinov/Form1.Designer.cs
@@ -28,7 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhoneBook));
             newEntryButton = new Button();
             firstNameTextBox = new TextBox();
             firstNameLabel = new Label();
@@ -42,7 +43,10 @@
             emailTextBox = new TextBox();
             emailLabel = new Label();
             contactsDataGrid = new DataGridView();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)contactsDataGrid).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // newEntryButton
@@ -52,8 +56,8 @@
             newEntryButton.FlatAppearance.BorderSize = 2;
             newEntryButton.FlatAppearance.MouseOverBackColor = Color.DarkSeaGreen;
             newEntryButton.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            newEntryButton.Location = new Point(54, 7);
-            newEntryButton.Margin = new Padding(2, 2, 2, 2);
+            newEntryButton.Location = new Point(11, 12);
+            newEntryButton.Margin = new Padding(2);
             newEntryButton.Name = "newEntryButton";
             newEntryButton.Size = new Size(123, 38);
             newEntryButton.TabIndex = 5;
@@ -64,16 +68,16 @@
             // firstNameTextBox
             // 
             firstNameTextBox.Cursor = Cursors.IBeam;
-            firstNameTextBox.Location = new Point(248, 56);
-            firstNameTextBox.Margin = new Padding(2, 2, 2, 2);
+            firstNameTextBox.Location = new Point(268, 37);
+            firstNameTextBox.Margin = new Padding(2);
             firstNameTextBox.Name = "firstNameTextBox";
-            firstNameTextBox.Size = new Size(282, 23);
+            firstNameTextBox.Size = new Size(360, 23);
             firstNameTextBox.TabIndex = 1;
             // 
             // firstNameLabel
             // 
             firstNameLabel.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            firstNameLabel.Location = new Point(150, 56);
+            firstNameLabel.Location = new Point(179, 41);
             firstNameLabel.Margin = new Padding(2, 0, 2, 0);
             firstNameLabel.Name = "firstNameLabel";
             firstNameLabel.Size = new Size(85, 19);
@@ -83,7 +87,7 @@
             // lastNameLabel
             // 
             lastNameLabel.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            lastNameLabel.Location = new Point(150, 91);
+            lastNameLabel.Location = new Point(179, 76);
             lastNameLabel.Margin = new Padding(2, 0, 2, 0);
             lastNameLabel.Name = "lastNameLabel";
             lastNameLabel.Size = new Size(85, 19);
@@ -94,29 +98,29 @@
             // 
             lastNameTextBox.Cursor = Cursors.IBeam;
             lastNameTextBox.ForeColor = SystemColors.WindowText;
-            lastNameTextBox.Location = new Point(248, 91);
-            lastNameTextBox.Margin = new Padding(2, 2, 2, 2);
+            lastNameTextBox.Location = new Point(268, 72);
+            lastNameTextBox.Margin = new Padding(2);
             lastNameTextBox.Name = "lastNameTextBox";
-            lastNameTextBox.Size = new Size(282, 23);
+            lastNameTextBox.Size = new Size(360, 23);
             lastNameTextBox.TabIndex = 2;
             // 
             // phoneNumberTextBox
             // 
             phoneNumberTextBox.Cursor = Cursors.IBeam;
             phoneNumberTextBox.ForeColor = SystemColors.WindowText;
-            phoneNumberTextBox.Location = new Point(248, 127);
-            phoneNumberTextBox.Margin = new Padding(2, 2, 2, 2);
+            phoneNumberTextBox.Location = new Point(268, 108);
+            phoneNumberTextBox.Margin = new Padding(2);
             phoneNumberTextBox.Name = "phoneNumberTextBox";
-            phoneNumberTextBox.Size = new Size(282, 23);
+            phoneNumberTextBox.Size = new Size(360, 23);
             phoneNumberTextBox.TabIndex = 3;
             // 
             // phoneNumberLabel
             // 
             phoneNumberLabel.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            phoneNumberLabel.Location = new Point(125, 127);
+            phoneNumberLabel.Location = new Point(148, 112);
             phoneNumberLabel.Margin = new Padding(2, 0, 2, 0);
             phoneNumberLabel.Name = "phoneNumberLabel";
-            phoneNumberLabel.Size = new Size(119, 19);
+            phoneNumberLabel.Size = new Size(116, 19);
             phoneNumberLabel.TabIndex = 6;
             phoneNumberLabel.Text = "Phone Number:";
             // 
@@ -127,8 +131,8 @@
             loadButton.FlatAppearance.BorderSize = 2;
             loadButton.FlatAppearance.MouseOverBackColor = Color.DarkSeaGreen;
             loadButton.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            loadButton.Location = new Point(216, 7);
-            loadButton.Margin = new Padding(2, 2, 2, 2);
+            loadButton.Location = new Point(11, 54);
+            loadButton.Margin = new Padding(2);
             loadButton.Name = "loadButton";
             loadButton.Size = new Size(123, 38);
             loadButton.TabIndex = 6;
@@ -144,8 +148,8 @@
             saveButton.FlatAppearance.MouseOverBackColor = Color.DarkSeaGreen;
             saveButton.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
             saveButton.ForeColor = SystemColors.Control;
-            saveButton.Location = new Point(394, 7);
-            saveButton.Margin = new Padding(2, 2, 2, 2);
+            saveButton.Location = new Point(11, 98);
+            saveButton.Margin = new Padding(2);
             saveButton.Name = "saveButton";
             saveButton.Size = new Size(123, 38);
             saveButton.TabIndex = 7;
@@ -161,8 +165,8 @@
             deleteButton.FlatAppearance.MouseOverBackColor = Color.DarkSeaGreen;
             deleteButton.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
             deleteButton.ForeColor = SystemColors.Control;
-            deleteButton.Location = new Point(571, 7);
-            deleteButton.Margin = new Padding(2, 2, 2, 2);
+            deleteButton.Location = new Point(11, 140);
+            deleteButton.Margin = new Padding(2);
             deleteButton.Name = "deleteButton";
             deleteButton.Size = new Size(123, 38);
             deleteButton.TabIndex = 8;
@@ -174,16 +178,16 @@
             // 
             emailTextBox.Cursor = Cursors.IBeam;
             emailTextBox.ForeColor = SystemColors.WindowText;
-            emailTextBox.Location = new Point(248, 162);
-            emailTextBox.Margin = new Padding(2, 2, 2, 2);
+            emailTextBox.Location = new Point(268, 143);
+            emailTextBox.Margin = new Padding(2);
             emailTextBox.Name = "emailTextBox";
-            emailTextBox.Size = new Size(282, 23);
+            emailTextBox.Size = new Size(360, 23);
             emailTextBox.TabIndex = 4;
             // 
             // emailLabel
             // 
             emailLabel.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            emailLabel.Location = new Point(171, 162);
+            emailLabel.Location = new Point(204, 149);
             emailLabel.Margin = new Padding(2, 0, 2, 0);
             emailLabel.Name = "emailLabel";
             emailLabel.Size = new Size(50, 19);
@@ -193,28 +197,57 @@
             // contactsDataGrid
             // 
             contactsDataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            contactsDataGrid.BorderStyle = BorderStyle.Fixed3D;
             contactsDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             contactsDataGrid.Location = new Point(8, 198);
-            contactsDataGrid.Margin = new Padding(2, 2, 2, 2);
+            contactsDataGrid.Margin = new Padding(2);
             contactsDataGrid.Name = "contactsDataGrid";
             contactsDataGrid.RowHeadersWidth = 62;
-            dataGridViewCellStyle1.BackColor = Color.AntiqueWhite;
-            dataGridViewCellStyle1.Font = new Font("Trebuchet MS", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            dataGridViewCellStyle1.ForeColor = Color.MediumBlue;
-            dataGridViewCellStyle1.SelectionBackColor = Color.LightGreen;
-            dataGridViewCellStyle1.SelectionForeColor = Color.Black;
-            contactsDataGrid.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.BackColor = Color.AntiqueWhite;
+            dataGridViewCellStyle2.Font = new Font("Trebuchet MS", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = Color.MediumBlue;
+            dataGridViewCellStyle2.SelectionBackColor = Color.LightGreen;
+            dataGridViewCellStyle2.SelectionForeColor = Color.Black;
+            contactsDataGrid.RowsDefaultCellStyle = dataGridViewCellStyle2;
             contactsDataGrid.RowTemplate.Height = 33;
-            contactsDataGrid.Size = new Size(764, 212);
+            contactsDataGrid.Size = new Size(821, 297);
             contactsDataGrid.TabIndex = 12;
             contactsDataGrid.CellMouseClick += contactsDataGrid_CellMouseClick;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.ActiveLinkColor = Color.OrangeRed;
+            linkLabel1.AutoSize = true;
+            linkLabel1.Cursor = Cursors.Hand;
+            linkLabel1.LinkColor = Color.SteelBlue;
+            linkLabel1.Location = new Point(653, 158);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(159, 15);
+            linkLabel1.TabIndex = 13;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "PhoneBook-by-I-M-Marinov";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Cursor = Cursors.Hand;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(667, 37);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(131, 129);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 14;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // PhoneBook
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Silver;
-            ClientSize = new Size(781, 417);
+            ClientSize = new Size(840, 506);
+            Controls.Add(linkLabel1);
+            Controls.Add(pictureBox1);
             Controls.Add(contactsDataGrid);
             Controls.Add(emailLabel);
             Controls.Add(emailTextBox);
@@ -229,11 +262,12 @@
             Controls.Add(firstNameTextBox);
             Controls.Add(newEntryButton);
             ForeColor = SystemColors.ControlText;
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "PhoneBook";
             Text = "PhoneBook-by-I-M-Marinov";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)contactsDataGrid).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -253,5 +287,7 @@
         private TextBox emailTextBox;
         private Label emailLabel;
         private DataGridView contactsDataGrid;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox1;
     }
 }
--- Phone-Book-by-I-M-Marinov/Form1.cs
+++ Phone-Book-by-I-M-Marinov/Form1.cs
@@ -1,18 +1,31 @@
+using OfficeOpenXml;
 using System.Data;
+
 
 namespace Phone_Book_by_I_M_Marinov
 {
     public partial class PhoneBook : Form
     {
+
+
+        string excelFilePath = @"C:\Users\Marinov\Desktop\contacts.xlsx";
+        DataTable contactsTable = new();
+        bool isEdited;
+        private int lastEntryIndex = -1;
+
         public PhoneBook()
         {
             InitializeComponent();
         }
 
-        DataTable contactsTable = new();
-        bool isEdited;
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeContactsTable();
+            LoadContactsFromExcel();
+        }
+
+        private void InitializeContactsTable()
         {
             contactsTable.Columns.Add("First Name");
             contactsTable.Columns.Add("Last Name");
@@ -31,9 +44,8 @@ namespace Phone_Book_by_I_M_Marinov
                 {
                     int rowIndex = contactsDataGrid.CurrentCell.RowIndex;
                     contactsTable.Rows.RemoveAt(rowIndex);
-                    contactsDataGrid.DataSource = contactsTable;
+                    SaveContactsToExcel();
 
-                    // After deletion, check if there are any rows left in the table
                     if (contactsTable.Rows.Count == 0)
                     {
                         ClearAllEntries();
@@ -41,7 +53,6 @@ namespace Phone_Book_by_I_M_Marinov
                     }
                     else
                     {
-                        // Ensure the DataGridView shows the last row after deletion
                         if (rowIndex >= contactsTable.Rows.Count)
                         {
                             rowIndex = contactsTable.Rows.Count - 1;
@@ -70,7 +81,7 @@ namespace Phone_Book_by_I_M_Marinov
                 string.IsNullOrWhiteSpace(emailTextBox.Text))
             {
                 MessageBox.Show("Please fill all the information before saving!");
-                return; 
+                return;
             }
 
             if (isEdited)
@@ -90,12 +101,14 @@ namespace Phone_Book_by_I_M_Marinov
             }
             else
             {
-                contactsTable.Rows.Add(
-                    firstNameTextBox.Text, 
-                    lastNameTextBox.Text, 
+                AddEntryToDataTable(
+                    firstNameTextBox.Text,
+                    lastNameTextBox.Text,
                     phoneNumberTextBox.Text,
                     emailTextBox.Text
                     );
+
+                SaveContactsToExcel();
             }
 
             ClearAllEntries();
@@ -104,11 +117,23 @@ namespace Phone_Book_by_I_M_Marinov
 
         private void loadButton_Click(object sender, EventArgs e)
         {
-            firstNameTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[0].ToString();
-            lastNameTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[1].ToString();
-            phoneNumberTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[2].ToString();
-            emailTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[3].ToString();
-            isEdited = true;
+            if (contactsDataGrid.CurrentCell != null && contactsDataGrid.CurrentCell.RowIndex >= 0)
+            {
+                int rowIndex = contactsDataGrid.CurrentCell.RowIndex;
+
+                if (rowIndex < contactsTable.Rows.Count)
+                {
+                    firstNameTextBox.Text = contactsTable.Rows[rowIndex].ItemArray[0].ToString();
+                    lastNameTextBox.Text = contactsTable.Rows[rowIndex].ItemArray[1].ToString();
+                    phoneNumberTextBox.Text = contactsTable.Rows[rowIndex].ItemArray[2].ToString();
+                    emailTextBox.Text = contactsTable.Rows[rowIndex].ItemArray[3].ToString();
+                    isEdited = true;
+                }
+                else
+                {
+                    ClearAllEntries();
+                }
+            }
         }
 
         private void newEntryButton_Click(object sender, EventArgs e)
@@ -118,11 +143,15 @@ namespace Phone_Book_by_I_M_Marinov
 
         private void contactsDataGrid_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
-            firstNameTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[0].ToString();
-            lastNameTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[1].ToString();
-            phoneNumberTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[2].ToString();
-            emailTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[3].ToString();
-            isEdited = true;
+            if (contactsDataGrid.CurrentCell != null && lastEntryIndex == contactsDataGrid.CurrentCell.RowIndex + 1)
+            {
+                firstNameTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[0].ToString();
+                lastNameTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[1].ToString();
+                phoneNumberTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[2].ToString();
+                emailTextBox.Text = contactsTable.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[3].ToString();
+                isEdited = true;
+            }
+
         }
 
         private void ClearAllEntries()
@@ -131,6 +160,103 @@ namespace Phone_Book_by_I_M_Marinov
             lastNameTextBox.Text = "";
             phoneNumberTextBox.Text = "";
             emailTextBox.Text = "";
+        }
+
+
+        private void LoadContactsFromExcel()
+        {
+            if (File.Exists(excelFilePath))
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFilePath)))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets["Contacts"];
+                    int rowCount = worksheet.Dimension.Rows;
+
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        contactsTable.Rows.Add(
+                            worksheet.Cells[row, 1].Value?.ToString(),
+                            worksheet.Cells[row, 2].Value?.ToString(),
+                            worksheet.Cells[row, 3].Value?.ToString(),
+                            worksheet.Cells[row, 4].Value?.ToString()
+                        );
+                    }
+                }
+            }
+        }
+
+        private void SaveContactsToExcel()
+        {
+            if (File.Exists(excelFilePath))
+            {
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFilePath)))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets["Contacts"];
+
+                    if (contactsTable.Rows.Count > 0)
+                    {
+                        if (worksheet.Dimension?.Rows > 1 && worksheet.Dimension?.Columns > 0)
+                        {
+                            worksheet.Cells["A2:D" + worksheet.Dimension.End.Row].Clear();
+                        }
+
+                        int rowIndex = 2;
+                        foreach (DataRow row in contactsTable.Rows)
+                        {
+                            if (row.RowState != DataRowState.Deleted)
+                            {
+                                worksheet.Cells[rowIndex, 1].Value = row["First Name"];
+                                worksheet.Cells[rowIndex, 2].Value = row["Last Name"];
+                                worksheet.Cells[rowIndex, 3].Value = row["Phone Number"];
+                                worksheet.Cells[rowIndex, 4].Value = row["Email"];
+                                rowIndex++;
+                            }
+                        }
+                    }
+
+                    if (contactsTable.Rows.Count == 0)
+                    {
+                        worksheet.Cells["A2:D" + worksheet.Dimension.End.Row].Clear();
+                    }
+
+                    package.Save();
+                }
+            }
+        }
+
+        private void AddEntryToDataTable(string firstName, string lastName, string phoneNumber, string email)
+        {
+            contactsTable.Rows.Add(firstName, lastName, phoneNumber, email);
+            lastEntryIndex = contactsTable.Rows.Count - 1;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://github.com/I-M-Marinov/Phone-Book-by-I-M-Marinov";
+
+            try
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening link: " + ex.Message);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string url = "https://github.com/I-M-Marinov/Phone-Book-by-I-M-Marinov";
+
+            try
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening link: " + ex.Message);
+            }
         }
     }
 }
